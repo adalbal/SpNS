e4bd435e929d7191ca2b26a14eab345ad17762f5
adalbal <adel.alsalti@upc.edu>
2023-05-11 20:09:22 +0200

    Fixed check.sh to only check output time and Ek

--- QA/check.bat
+++ QA/check.bat
@@ -5,6 +5,6 @@ for X in `ls -d TEST_*` ; do
   echo "==============================================================================="
   echo $X 
   echo "==============================================================================="
-  cat stdout.0 | grep Time > 1 ; cat REF/stdout.0 | grep Time > 2; diff 1 2; rm -rf Time 1 2;
+  cat stdout.0 | grep "Time:" > 1 ; cat REF/stdout.0 | grep "Time:" > 2; diff 1 2; rm -rf Time 1 2;
   cd ..
 done
